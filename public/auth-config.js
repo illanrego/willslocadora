10e742c6e8eef76aa4bf0a984c88813f802c8680
illanrego <emaildoillan@protonmail.com>
2026-07-31 05:16:29 -0300

    feat: public keys add

--- public/auth-config.js
+++ public/auth-config.js
@@ -1,6 +1,6 @@
 /* Deployment-time public configuration. Leave blank to keep anonymous browsing enabled. */
 window.LocadoraAuthConfig = Object.freeze({
-  clerkPublishableKey: '',
-  clerkFrontendApi: '',
-  dataApiBase: '',
+  clerkPublishableKey: 'pk_test_c3dlZXBpbmctbmV3dC0zOS5jbGVyay5hY2NvdW50cy5kZXYk',
+  clerkFrontendApi: 'https://sweeping-newt-39.clerk.accounts.dev',
+  dataApiBase: 'https://locadora-data.willstartpage.workers.dev',
 });
